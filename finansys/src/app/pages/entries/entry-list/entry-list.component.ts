10f98865b9325e3695feea32dcc88632cd65cd3b
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-03-16 19:05:27 -0300

    Updates finansys project

--- finansys/src/app/pages/entries/entry-list/entry-list.component.ts
+++ finansys/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,37 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Entry } from '../shared/entry.model';
-import { EntryService } from '../shared/entry.service';
+import { BaseResourceListComponent } from "../../../shared/components/base-resource-list/base-resource-list.component";
+
+import { Entry } from "../shared/entry.model";
+import { EntryService } from "../shared/entry.service";
 
 @Component({
 	selector: 'app-entry-list',
 	templateUrl: './entry-list.component.html',
 	styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-	entries: Entry[] = [];
-
-	constructor(
-		private entryService: EntryService
-	) { }
-
-	ngOnInit(): void {
-		this.entryService.getAll().subscribe(
-			entries => this.entries = entries.sort((a,b) => b.id - a.id),
-			error => alert('Erro ao carregar a lista')
-		)
+	constructor(private entryService: EntryService) {
+		super(entryService);
 	}
-
-	deleteEntry(entry) {
-		const mustDelete = confirm('Deseja realmente excluir este item?');
-		
-		if (mustDelete){
-		  this.entryService.delete(entry.id).subscribe(
-			() => this.entries = this.entries.filter(element => element != entry),
-			() => alert("Erro ao tentar excluir!")
-		  )
-		}
-	  }
 
 }
--- finansys/src/app/shared/shared.module.ts
+++ finansys/src/app/shared/shared.module.ts
@@ -1,32 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms";
-//import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
-//import { PageHeaderComponent } from './components/page-header/page-header.component';
-//import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
-//import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
+import { RouterModule } from "@angular/router";
 
-
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-	declarations: [
-		//BreadCrumbComponent,
-		//PageHeaderComponent,
-		//FormFieldErrorComponent,
-		//ServerErrorMessagesComponent
-	],
 	imports: [
 		CommonModule,
-		ReactiveFormsModule		
+		ReactiveFormsModule,
+		RouterModule
+	],
+	declarations: [
+		BreadCrumbComponent,
+		PageHeaderComponent,
 	],
 	exports: [
 		// shared modules
 		CommonModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
+		RouterModule,
 
 		// shared components
-		//BreadCrumbComponent,
-		//PageHeaderComponent,
+		BreadCrumbComponent,
+		PageHeaderComponent,
 		//FormFieldErrorComponent,
 		//ServerErrorMessagesComponent
 	]
